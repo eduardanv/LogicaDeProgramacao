da682b3520394fe1ab2fb97250e99cf4a19160da
Eduarda Neves <99812457+eduardanv@users.noreply.github.com>
2022-08-03 14:25:05 -0300

    primeiro exercicio lp
    primeira aplicação para treino de java na ide netbeans

---
+++ primeira-aplicacao-java/src/main/java/com/mycompany/primeira/aplicacao/java/PrimeiraClasse.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.primeira.aplicacao.java;
+
+/**
+ *
+ * @author aluno
+ */
+public class PrimeiraClasse {
+
+    //Método (principal)
+    public static void main(String[] args) {
+
+        var frase = "Hello World!";
+        var conta = 1 + 2;
+        System.out.println(frase);
+        System.out.println(conta);
+
+    }
+
+}
